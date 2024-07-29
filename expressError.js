fdd49bf5265a747096b8c55fb2f71ce616246fc1
Rahul1532 <pandeyrahul1532@gmail.com>
2024-07-29 07:39:42 +0530

    new commit

---
+++ expressError.js
@@ -0,0 +1,11 @@
+const { listSearchIndexes } = require("./models/chat");
+
+class expressError extends Error{
+    constructor(status, message) {
+        super();
+        this.status = status;
+        this.message = message;
+    }
+}
+
+module.exports = expressError;
